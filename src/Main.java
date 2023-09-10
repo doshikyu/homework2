cca23fe5b0a44d576a038d5d467a70ef98ee6008
doshikyu <yu.dashik@gmail.com>
2023-09-10 23:17:55 +0900

    - Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        var dog = 8.0;
+        System.out.println("dog = " + dog);
+        var cat = 3.6;
+        System.out.println("cat = " + cat);
+        var paper = 763789;
+        System.out.println("paper = " + paper);
+
+        System.out.println("\nЗадача 2");
+        dog = dog + 4;
+        System.out.println("dog = " + dog);
+        cat = cat + 4;
+        System.out.println("cat = " + cat);
+        paper = paper + 4;
+        System.out.println("paper = " + paper);
+
+        System.out.println("\nЗадача 3");
+        dog = dog - 3.5;
+        System.out.println("dog = " + dog);
+        cat  = cat - 1.6;
+        System.out.println("cat = " + cat);
+        paper  = paper - 7639;
+        System.out.println("paper = " + paper);
+
+        System.out.println("\nЗадача 4");
+        var friend = 19;
+        System.out.println("friend = " + friend);
+        friend = friend + 2;
+        System.out.println("friend = " + friend);
+        friend = friend / 7;
+        System.out.println("friend = " + friend);
+
+        System.out.println("\nЗадача 5");
+        var frog = 3.5;
+        System.out.println("frog = " + frog);
+        frog = frog * 10;
+        System.out.println("frog = " + frog);
+        frog = frog / 3.5;
+        System.out.println("frog = " + frog);
+        frog = frog + 4;
+        System.out.println("frog = " + frog);
+
+    }
+}
